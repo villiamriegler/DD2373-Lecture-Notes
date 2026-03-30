128fc6952e1a79d762a978b15f3a8da9fce2b8a3
Villiam Riegler <ville@riegler.se>
2026-03-30 13:11:47 +0200

    setup local

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+            ltex-ls-plus
+            zathura
+            texliveFull
+        ];
+    };
+  };
+}
